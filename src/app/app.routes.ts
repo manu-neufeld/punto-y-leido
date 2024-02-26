f7ef9c93e644abe317ed1dedd5d817f793275c86
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-02-26 15:27:56 +0100

    publish branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export const routes: Routes = [];
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
+export const routes: Routes = [
+    // { path: '', component: HomeComponent },
+    // { path: 'map/:id', component: MapComponent },
+    // { path: 'data/:id', component: DataComponent, canActivate: [authGuard] },
+    // { path: environment.newmap_url, component: MapComponent, canActivate: [authGuard] }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes),],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
