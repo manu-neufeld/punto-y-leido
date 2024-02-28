97d9537b078847ba3a768ad03d9a7b774426a604
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-02-28 12:33:06 +0100

    footer created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,25 +8,24 @@ import { WritingComponent } from './writing/writing.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { InstagramComponent } from './instagram/instagram.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { VideoComponent } from './video/video.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http);
 }
 
-
 export const routes: Routes = [
-    {path: '', redirectTo: '/about-me', pathMatch: 'full'},
-    {path: 'about-me', component: AboutMeComponent},
-    {path: 'writing', component: WritingComponent},
-    {path: 'reviews', component: ReviewsComponent},
-    {path: 'social', component: InstagramComponent},
-    {path: 'contact', component: ContactComponent},
+  { path: '', redirectTo: '/about-me', pathMatch: 'full' },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'writing', component: WritingComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'social', component: InstagramComponent },
+  { path: 'video', component: VideoComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes),],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
