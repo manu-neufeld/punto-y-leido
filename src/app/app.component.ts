cf88eed3ed6bc3dcd9f2d9b8928e3a2293fcbc00
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-03-04 13:38:12 +0100

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { WorkInProgressComponent } from "./work-in-progress/work-in-progress.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, NgbModule, FooterComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, NgbModule, FooterComponent, WorkInProgressComponent]
 })
 export class AppComponent {
   title = 'punto-y-leido';
