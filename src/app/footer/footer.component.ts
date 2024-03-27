458eebdb4a39b7646caefb7beba614b72b3ae60e
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-03-27 13:49:06 +0100

    footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faImages, faMailBulk, faRss, faVideo, } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  blog = faRss
+  instagram = faImages
+  youtube = faVideo
 }
