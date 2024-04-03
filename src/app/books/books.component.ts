88514dd41a14492e1f89edf221820af189fcfe2e
manuela r. neufeld <m.rodriguez@nevintec.com>
2024-04-03 13:04:14 +0200

    books base created

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-books',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './books.component.scss'
 })
 export class BooksComponent {
+
+  public shelves: any = {
+    favorites: 0,
+    toRead: 2,
+    readingNow: 3,
+    haveRead: 4,
+    books2023: 1002,
+    books2024: 1003,
+    myShelf: 1001
+  }
+
+  constructor(private service: BooksService){
+  }
+
+  ngOnInit(){
+    this.service.getBooksShelf(this.shelves.favorites).subscribe((ret: any) =>{
+      if(ret){
+        console.log(ret);
+        
+      }
+    })
+  }
 
 }
---
+++ src/services/books.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, map, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor(private http: HttpClient) { }
+
+  public urlBooks: string = "https://www.googleapis.com/books/v1/users/111053013139583379494/bookshelves/";
+  public volumesKey: string = "/volumes?key=AIzaSyDVgtUTxsRq9GSbiB0Q5isYrHIbpDpSBn8";
+
+  public getBooksShelf(shelf: string): Observable<any>{
+    return this.http.get<any>(this.urlBooks + shelf + this.volumesKey ).pipe(
+      map((data: any) => {
+        console.log(data);
+        return data
+      }),
+      catchError((err) => {
+        console.log(err);
+        return of(undefined);
+      })
+    )
+  }
+}
